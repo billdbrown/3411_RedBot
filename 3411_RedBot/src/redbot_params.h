4938854f0ae57ac9cf2f8d427dc98513f2dfd0cd
Bill <williamdbrown96@gmail.com>
2018-03-20 13:59:08 -0400

    added parameter header file

---
+++ 3411_RedBot/src/redbot_params.h
@@ -0,0 +1,18 @@
+/*
+ * redbot_params.h
+ *
+ * Created: 3/20/2018 9:46:32 AM
+ *  Author: Bill
+ */ 
+
+
+#ifndef REDBOT_PARAMS_H_
+#define REDBOT_PARAMS_H_
+
+// Motor Speed Definitions
+#define START_SPEED		0.05
+
+
+
+
+#endif /* INCFILE1_H_ */
